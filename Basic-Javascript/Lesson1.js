2b8ad7222e33a9601b3c13de6a131b8993eea3cc
Romi123 <romicoolvarghese'@gmail.com>
2024-07-04 20:34:59 +0400

    Variable & Concatination of files

---
+++ Basic-Javascript/Lesson1.js
@@ -0,0 +1,8 @@
+var fName = "Romi"
+var lName = "Varghese"
+var output = "My Name is "+fName+" "+lName
+console.log(output)
+var fName = "Sijo"
+var lName = "Varghese"
+var output = "My Name is "+fName+" "+lName
+console.log(output)
